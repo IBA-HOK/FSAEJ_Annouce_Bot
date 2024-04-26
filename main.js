b58a27477bc823138590f54a04ee329af40815fb
IBA-HOK <231205026@ccmailg.meijo-u.ac.jp>
2024-04-26 14:24:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,70 @@
 const request = require('request');
 const parse = require("node-html-parser").parse;
 const fs = require('fs');
-const env = require('dotenv').config();
+require('dotenv').config();
 const options = {
     url: 'https://www.jsae.or.jp/formula/',
     method: 'GET'
 };
-request(options, function (error, response, body) {
-    let root = parse(body)
-    let processor = []
-    const h2 = root?.querySelectorAll(".newsList_title");
-    for (let i = 0; i < h2.length; i++) {
-        processor.push([h2[i].innerText.trim(), h2[i].getAttribute('href')]);//trim()で前後の空白を削除
-    }
-    let path = "./output.json"
-    let input;
-    try{
-        input = JSON.parse(fs.readFileSync(path, 'utf8'))
-    }catch (e){
-        input = []
-    }
-    //inputにnewsを追記
-    let postData;
-    for (let i = 0; i < processor.length; i++) {
-        if (!input.some((element) => element[0] === processor[i][0])) {
-            input.push(processor[i]);
-            postData = {
-                username: 'JSAE Announce BOT',
-                content: `${processor[i][0]}\n${processor[i][1]}`
-            }
-            fetch(process.env.discord_hook, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(postData)
-            })
-            console.log(postData)
-        }
-    }
-    fs.writeFile('output.json', JSON.stringify(input), (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
-})
 
+function htmlSpecialParse(text) {
+    return text.split("&amp;").join("&").split("&lt;").join("<").split("&gt;").join(">").split("&quot;").join("\"").split("&#39;").join("'").split("&nbsp;").join(" ");
+}
+
+function main() {
+    request(options, function (error, response, body) {
+        let root = parse(body)
+        let processor = []
+        const TITLE = root?.querySelectorAll(".newsList_title");
+        const TIME = root?.querySelectorAll(".newsList_dates");
+        for (let i = 0; i < TITLE.length; i++) {
+            processor.push({
+                "name": TITLE[i].innerText.trim(),
+                "url": TITLE[i].getAttribute('href'),
+                "time": TIME[i].innerText.trim()
+            });//trim()で前後の空白を削除
+        }
+        let path = "./output.json"
+        let input;
+        try {
+            input = JSON.parse(fs.readFileSync(path, 'utf8'))
+        } catch (e) {
+            input = []
+        }
+        let isNew = true;
+        //inputにnewsを追記
+        let postData;
+        for (let i = 0; i < processor.length; i++) {
+            for (let j = 0; j < input.length; j++) {
+                isNew = true;
+                if (processor[i].name === input[j].name && processor[i].time === input[j].time) {
+                    isNew = false;
+                    break;
+                }
+            }
+            if (isNew) {
+                input.push(processor[i]);
+                //postTest.push(processor[i]);
+                postData = {
+                    username: 'JSAE Announce BOT',
+                    content: `${htmlSpecialParse(processor[i].name)}\n${processor[i].url}`
+                }
+                console.log(postData)
+                fetch(process.env.discord_hook, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(postData)
+                })
+            }
+        }
+        fs.writeFile('output.json', JSON.stringify(input), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    })
+}
+
+main()
